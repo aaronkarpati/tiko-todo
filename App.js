d4c05511c69c766129dd368c4aa9ce4a3f9d16e3
aaronkarpati <karpatiaron@me.com>
2022-05-05 18:46:18 +0200

    cleaning up

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, {useContext, useState, useCallback, useEffect} from 'react';
-import {View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
